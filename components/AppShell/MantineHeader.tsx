2100e8ada0874d239f52a92791697a352f15c8a4
gage <gvankuilenburg@mail.valenciacollege.edu>
2023-03-19 05:28:43 +0000

    Update 9 files

---
+++ components/AppShell/MantineHeader.tsx
@@ -0,0 +1,162 @@
+import {
+  createStyles,
+  Header,
+  HoverCard,
+  Group,
+  Button,
+  UnstyledButton,
+  Text,
+  SimpleGrid,
+  ThemeIcon,
+  Anchor,
+  Divider,
+  Center,
+  Box,
+  Burger,
+  Drawer,
+  Collapse,
+  ScrollArea,
+  rem,
+} from '@mantine/core';
+import { MantineLogo } from '@mantine/ds';
+import { useDisclosure } from '@mantine/hooks';
+import {
+  IconNotification,
+  IconCode,
+  IconBook,
+  IconChartPie3,
+  IconFingerprint,
+  IconCoin,
+  IconChevronDown,
+} from '@tabler/icons-react';
+import { ColorSchemeToggle } from './../ColorSchemeToggle/ColorSchemeToggle';
+import { GiWaveCrest } from 'react-icons/gi';
+const useStyles = createStyles((theme) => ({
+  hiddenMobile: {
+    [theme.fn.smallerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  hiddenDesktop: {
+    [theme.fn.largerThan('sm')]: {
+      display: 'none',
+    },
+  },
+}));
+
+const mockdata = [
+  {
+    icon: IconCode,
+    title: 'Open source',
+    description: 'This Pokémon’s cry is very loud and distracting',
+  },
+  {
+    icon: IconCoin,
+    title: 'Free for everyone',
+    description: 'The fluid of Smeargle’s tail secretions changes',
+  },
+  {
+    icon: IconBook,
+    title: 'Documentation',
+    description: 'Yanma is capable of seeing 360 degrees without',
+  },
+  {
+    icon: IconFingerprint,
+    title: 'Security',
+    description: 'The shell’s rounded shape and the grooves on its.',
+  },
+  {
+    icon: IconChartPie3,
+    title: 'Analytics',
+    description: 'This Pokémon uses its flying ability to quickly chase',
+  },
+  {
+    icon: IconNotification,
+    title: 'Notifications',
+    description: 'Combusken battles with the intensely hot flames it spews',
+  },
+];
+
+export function MantineHeader() {
+  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
+  const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
+  const { classes, theme } = useStyles();
+
+  const links = mockdata.map((item) => (
+    <UnstyledButton className={classes.subLink} key={item.title}>
+      <Group noWrap align="flex-start">
+        <ThemeIcon size={34} variant="default" radius="md">
+          <item.icon size={rem(22)} color={theme.fn.primaryColor()} />
+        </ThemeIcon>
+        <div>
+          <Text size="sm" fw={500}>
+            {item.title}
+          </Text>
+          <Text size="xs" color="dimmed">
+            {item.description}
+          </Text>
+        </div>
+      </Group>
+    </UnstyledButton>
+  ));
+
+  return (
+    <Box pb={5}>
+      <Header height={60} px="md">
+        <Group position="apart" sx={{ height: '100%' }}>
+          <Text fz="lg" fw={1000} inherit variant="gradient" component="span">
+            Waves
+          </Text>
+
+          <Group sx={{ height: '100%' }} spacing={0} className={classes.hiddenMobile}></Group>
+
+          <Group className={classes.hiddenMobile}>
+            <ColorSchemeToggle />
+            <Button variant="default">Log in</Button>
+            <Button
+              leftIcon={<GiWaveCrest size="1rem" />}
+              variant="gradient"
+              gradient={{ from: 'cyan', to: 'indigo' }}
+            >
+              Sign up
+            </Button>
+          </Group>
+
+          <Burger opened={drawerOpened} onClick={toggleDrawer} className={classes.hiddenDesktop} />
+        </Group>
+      </Header>
+
+      <Drawer
+        opened={drawerOpened}
+        onClose={closeDrawer}
+        size="100%"
+        padding="md"
+        title={
+          <Text fz="lg" fw={1000} inherit variant="gradient" component="span">
+            Waves
+          </Text>
+        }
+        className={classes.hiddenDesktop}
+        zIndex={1000000}
+      >
+        <ScrollArea h={`calc(100vh - ${rem(60)})`} mx="-md">
+          <Divider my="sm" color={theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'} />
+
+          <Divider my="sm" color={theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'} />
+
+          <Group position="center" grow pb="xl" px="md">
+            <Button variant="default">Log in</Button>
+            <Button
+              leftIcon={<GiWaveCrest size="1rem" />}
+              variant="gradient"
+              gradient={{ from: 'cyan', to: 'indigo' }}
+            >
+              Sign up
+            </Button>
+          </Group>
+        </ScrollArea>
+      </Drawer>
+    </Box>
+  );
+}
